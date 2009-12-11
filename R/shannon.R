81f4cde397a801fc3c0d98d843eac120f3c83b07
Patrick Giraudoux <patrick.giraudoux@univ-fcomte.fr>
2009-12-11 07:03:56 +0000

    version 1.4.3

--- R/shannon.R
+++ R/shannon.R
@@ -1,10 +1,10 @@
-"shannon" <-
-function(vect, proba=TRUE){
-    if (!is.vector(vect)) stop("vector expected")
-    if (!proba) vect<-vect/sum(vect)
-    if (!all.equal(sum(vect),1)) stop("probability vector must sum to 1")
-    vect<-vect*log(vect,2)
-    h<-sum(vect[is.finite(vect)])
-    hmax<-log(1/length(vect),2)
-    c(H=-h,J=h/hmax)
-}
+shannon <- function (vect, proba = TRUE){
+  vect <- as.numeric (vect)
+  if (all (vect <= 0)) return (c(H=NA, J=NA))
+  if(!proba) vect <- vect/sum(vect)
+  if (!isTRUE(all.equal(sum(vect), 1))) stop("probability vector must sum to 1")
+    vect <- vect * log(vect, 2)
+    h <- sum(vect[is.finite(vect)])
+    hmax <- log(1/length(vect), 2)
+    return (c(H = -h, J = h/hmax))
+  }
