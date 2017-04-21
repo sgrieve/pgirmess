18a80e2e2fd5576bdf07cc8cc8f4b5aa72f0cedc
sgrieve <s.grieve@ed.ac.uk>
2017-04-21 13:34:36 +0100

    debugging

--- R/correlog.R
+++ R/correlog.R
@@ -4,7 +4,7 @@ function(coords,z,method="Moran",nbclass=NULL,...){
     matdist<-dist(coords)
     if (is.null(nbclass)) nbclass<-nclass.Sturges(matdist)
     etendue<-range(matdist)
-    breaks1<-seq(etendue[1],etendue[2],l=50)
+    breaks1<-seq(etendue[1],etendue[2],l=500)
     breaks2<-breaks1+0.000001
     breaks<-cbind(breaks1[1:length(breaks1)-1],breaks2[2:length(breaks2)])
     breaks[1,1] <- breaks[1,1] - 1e-6 # to avoid exclusion of points on the limit (Colin Beale)
